4bf04432affd1a104f3db915cec882d9cdd8be2d
Cereal <32020034+ce-real@users.noreply.github.com>
2017-12-09 18:47:27 -0500

    fourth update

---
+++ resources/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplayL true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+});
+
+var scroll = 0;
+
+$(window).scroll(function){
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop - scroll > 50){
+    var navbarHeight =
+    $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navbarHeight}, 150);
+    scroll = scrollTop;
+  }else if (scroll - scrollTop > 50){
+    $('.navbar').animate({top: '0px'}, 150);
+    scroll = scrollTop;
+  }else{
+    return false;
+  }
+}
